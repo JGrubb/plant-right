eaa0410af7e42fa3b2e9f5c0e940f9b15554591d
JGrubb <therealjohnnygrubb@gmail.com>
2010-03-21 21:38:45 -0400

    adding to user model

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081106072031) do
+ActiveRecord::Schema.define(:version => 20100321222606) do
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
@@ -24,16 +24,21 @@ ActiveRecord::Schema.define(:version => 20081106072031) do
   create_table "users", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "login",                            :null => false
-    t.string   "crypted_password",                 :null => false
-    t.string   "password_salt",                    :null => false
-    t.string   "persistence_token",                :null => false
-    t.integer  "login_count",       :default => 0, :null => false
+    t.string   "login",                                :null => false
+    t.string   "crypted_password",                     :null => false
+    t.string   "password_salt",                        :null => false
+    t.string   "persistence_token",                    :null => false
+    t.integer  "login_count",       :default => 0,     :null => false
     t.datetime "last_request_at"
     t.datetime "last_login_at"
     t.datetime "current_login_at"
     t.string   "last_login_ip"
     t.string   "current_login_ip"
+    t.boolean  "has_passed_quiz",   :default => false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.boolean  "is_admin",          :default => false
+    t.string   "email"
   end
 
   add_index "users", ["last_request_at"], :name => "index_users_on_last_request_at"
