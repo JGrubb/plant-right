e32d2a7da03e2e2f9b2a961772a5289702556f98
JGrubb <therealjohnnygrubb@gmail.com>
2010-03-23 10:33:19 -0400

    added inro content.  Getting in to the quiz now.  bon voyage

---
+++ app/controllers/survey_controller.rb
@@ -0,0 +1,2 @@
+class SurveyController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
-  map.root :controller => "main", :action => "intro"
+  map.root :controller => "survey", :action => "intro"
 end
