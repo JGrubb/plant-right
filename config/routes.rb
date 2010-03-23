3e765c5f0a1cd44ee2f6193872aa987ec4aea073
JGrubb <therealjohnnygrubb@gmail.com>
2010-03-23 00:29:28 -0400

    renamed welcome controller, started adding content, set root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.connect  ':controller/:action/:id'
+  map.connect  ':controller/:action/:id.:format'
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new"
+  map.root :controller => "main", :action => "intro"
 end
