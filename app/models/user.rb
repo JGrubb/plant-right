3bce4eff301f4ee80cca582c9a6d037178d00628
Jay Sanders <mindtonic@gmail.com>
2010-03-25 10:14:06 -0400

    Cleaning up User model and adding named scope.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,26 +4,15 @@ class User < ActiveRecord::Base
   validates_presence_of  :email, :last_name, :first_name, :county, :referral,
                         :message => "All fields are required."
 
-#  I've tried it all.  self.has_passed_quiz User.has_passed_quiz @user.has_passed_quiz
-#  and all I get is the same "undefined method error"
-#  So where am I supposed to be defining this f-ing method?
-   def member_status
-     @user.has_passed_quiz
-   end
-  
-   def admin_status
-     @user.is_admin
-   end
+
+	# These do the same thing.  Use the named scope in a controller like this: @users = User.members.all
+	named_scope :members, :order => "has_passed_quiz, last_name DESC"
    
-   def has_passed_quiz
-     @user.has_passed_quiz
-   end
-   
-# Would love to have a list of members so I can maunally set their has_passed_quiz value manually
-# since I've barely even touched the quiz.  Yes, I'm a little discouraged.
-# Not sure how to corale this into a view, though. 
-   def member_list
-     list = User.find(:all, :order => "has_passed_quiz, last_name DESC")
-    end
-     
+	def member_list
+		list = User.find(:all, :order => "has_passed_quiz, last_name DESC")
+	end
+	
+	def admin_status
+		is_admin
+	end
 end
