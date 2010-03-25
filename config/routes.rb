8dbb72511050f41b234d868d595032cd2fd74495
Jay Sanders <mindtonic@gmail.com>
2010-03-25 10:11:19 -0400

    Cleaning the Routing File

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,25 @@
 ActionController::Routing::Routes.draw do |map|
+  
+	# QUIZ ROUTES
+  map.quiz  "quiz", :controller => "quiz", :action => "index"
+  
+  # SURVEY ROUTES
+  map.survey_intro "survey/introduction", :controller => "survey", :action => "intro"
+  
+  # AUTHENTICATION ROUTES
+  map.user_redirect "user_redirect", :controller => "user_sessions", :action => "redirect"
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
-  map.connect  ':controller/:action/:id'
-  map.connect  ':controller/:action/:id.:format'
-  map.quiz  "quiz", :controller => "quiz", :action => "welcome"
   map.register "register", :controller => "users", :action => "new"
   map.resource :account, :controller => "users"
   map.resources :users
   map.resources :password_resets
   map.resource :user_session
+  
+  # ROOT ROUTE
   map.root :controller => "survey", :action => "intro"
+  
+  # DEFAULT ROUTES
+  map.connect  ':controller/:action/:id'
+  map.connect  ':controller/:action/:id.:format'  
 end
