5a9192fd13db12b23013da0ef198e5fa22bb18a4
JGrubb <therealjohnnygrubb@gmail.com>
2010-03-26 13:37:30 -0400

    trying to get downloads happening

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,9 +1,15 @@
 class SurveyController < ApplicationController
-  before_filter :has_passed_quiz, :only => :participants
+  before_filter :member_status, :only => :participants
   
 	def intro
 	end
 	
 	def participants
 	end
+	
+	def survey_download
+	  send_file 'public/files/survey_materials.zip', :type => "application/zip", :x_sendfile => true, :filename => "PlantRight Survey Materials.zip"
+    redirect_to participants_path
+	end
+	
 end
